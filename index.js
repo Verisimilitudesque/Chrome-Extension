385422275dbf622f8c9c78814e5c77961ac8e7fc
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-07 09:41:28 -0600

    Added delete button, delete function, storage function

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-// Extension variables
+// Extension variables & arrays
 let myLeads = []
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const clearBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+//Truthy falsy check for leadsFromLocalStorage
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-function renderLeads() {
+// Renders leads to the unordered list
+function render(leads) {
     // 1. Loop through the myLeads array and create an li for each item
     // 2. Add the item to the listItems variable instead of the ulEl.innerHTML
     // 3. Render the listItems inside the unordered list using ulEl.innerHTML
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         //listItems += "<li><a href=' " + myLeads[i] + " ' target='blank'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
-                <a target='blank' href='${myLeads[i]}' >
-                    ${myLeads[i]}
+                <a target='blank' href='${leads[i]}' >
+                    ${leads[i]}
                 </a>
             </li>
             `
@@ -28,3 +32,29 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
+
+// Delete all leads when clear button is double clicked
+clearBtn.addEventListener( "dblclick", function() { 
+
+    // Clear the myLeads array
+    myLeads = []
+    // Clear the localStorage
+    localStorage.clear()
+    // Render the updated list
+    render(myLeads)
+
+})
+
+// Saves leads from input field to myLeads array and localStorage
+inputBtn.addEventListener("click", function() {
+    // Get the value from the input field
+    // Push it to myLeads array
+    // Call renderLeads() to display the updated list
+    myLeads.push(inputEl.value)
+    inputEl.value = "" // Clear the input field after adding
+    localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Save to localStorage)
+    render(myLeads)
+
+})
+
