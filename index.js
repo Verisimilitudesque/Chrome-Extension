4c14aa4cd48e08dae355849e98892f4f400e26bf
Jon <140838296+Verisimilitudesque@users.noreply.github.com>
2025-05-12 10:54:16 -0600

    Firebase database and app integration

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.7.1/firebase-database.js"
+
+const firebaseConfig = {
+    databaseURL: process.env.DATABASE_URL
+}
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+
+console.log(firebaseConfig.databaseURL)
+
 // Extension variables & arrays
-let myLeads = []
-let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const clearBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-
-//Truthy falsy check for leadsFromLocalStorage
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
 // Renders leads to the unordered list
 function render(leads) {
@@ -36,35 +37,18 @@ function render(leads) {
 
 // Saves leads from input field to myLeads array and localStorage
 inputBtn.addEventListener("click", function() {
-    // Get the value from the input field
-    // Push it to myLeads array
-    // Call renderLeads() to display the updated list
-    myLeads.push(inputEl.value)
+
+    console.log(inputEl.value)
     inputEl.value = "" // Clear the input field after adding
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Save to localStorage)
-    render(myLeads)
 
 })
 
 
-// Saves lead from the current tab to myLeads array and localStorage
-tabBtn.addEventListener("click", function() {
-    // Grab the current tab URL using the Chrome Tabs API
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          // Get the current tab URL and push it to myLeads array
-            myLeads.push(tabs[0].url)
-            localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Save to localStorage)
-            render(myLeads)
-    })
-})
+
 
 // Delete all leads when clear button is double clicked
 clearBtn.addEventListener( "dblclick", function() { 
 
-    // Clear the myLeads array
-    myLeads = []
-    // Clear the localStorage
-    localStorage.clear()
     // Render the updated list
     render(myLeads)
 
